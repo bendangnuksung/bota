b97dbe08f3509a457ecc5b498b187b35d0f4c1b9
bendangnuksung <bendangnuksungimsong@gmail.com>
2019-08-02 17:48:46 +0530

    background scrap mode 2 in run_bota.sh

--- run_bota.sh
+++ run_bota.sh
@@ -20,7 +20,7 @@ screen -X -S scrap quit
 
 # Run process
 screen -S bota -d -m python3 bota/main.py
-screen -S scrap -d -m python3 bota/background_scrap.py
+screen -S scrap -d -m python3 bota/background_scrap.py -m 2
 echo "Running main.py, background_scrap.py in background"
 echo "Type 'screen -r bota' to check bota process"
 echo "Tyoe 'screen -r scrap' to check background process"
