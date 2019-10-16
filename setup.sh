316eb68b5f4e944fe4d6903e0583751135ac7c1b
bendangnuksung <bendangnuksungimsong@gmail.com>
2019-10-16 12:52:35 +0530

    add pyppeteer dependcies

--- setup.sh
+++ setup.sh
@@ -41,3 +41,6 @@ cd dota2api
 sudo python3 setup.py install
 cd ../
 sudo rm -rf dota2api
+
+echo "Pyppeteer dependencies installation...."
+sudo apt-get update && sudo apt-get install -yq --no-install-recommends libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 libnss3 
