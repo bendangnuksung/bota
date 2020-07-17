2cea4b385142fe68245852b5010a8184cf672d41
bendangnuksung <bendangnuksungimsong@gmail.com>
2020-07-18 03:49:56 +0530

    print only called commands

--- bota/main.py
+++ bota/main.py
@@ -694,7 +694,7 @@ async def on_message(message):
 
     if is_command_called:
         log_caller.save_log(message, command_called)
-
+        print(f"{message.author.name}: {message.author}: {message.channel}: {message_content}")
 
 
     # Getting total guilds using
