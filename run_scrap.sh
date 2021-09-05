157730f25435d5f33dd21b1760065e88a6ed0a0f
bendang <bendangnuksungimsong@gmail.com>
2021-09-05 13:30:37 +0530

    increase sleep to 2 min

--- run_scrap.sh
+++ run_scrap.sh
@@ -5,10 +5,10 @@ if [ -z "$1" ]
   then
     while true
     do
+      echo "mode 2"
       python3 bota/background_scrap.py -m 2
-      sleep 30
+      sleep 120
     done
 else
   python3 bota/background_scrap.py -m 3
 fi
-
